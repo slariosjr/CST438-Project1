73d073b086336671f7e32878efe976ef7375c348
Alexander Betancourt <thelm900@gmail.com>
2024-09-05 11:45:36 -0700

    commit from laptop 11:45 am
    Transfer to PC

--- Project_1/app/(tabs)/nearby.tsx
+++ Project_1/app/(tabs)/nearby.tsx
@@ -6,10 +6,11 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { requestPermsBluetooth } from '@/lib/bluetooth';
 
 const setUpBluetoothConnection = () => {
-  console.log("The button works!");
-}
+    requestPermsBluetooth();
+} 
 
 export default function TabTwoScreen() {
   return (
