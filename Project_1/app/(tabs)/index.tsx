2fbab61644bd29de2dd9b53d04071903a3d90ebd
Silvia Pineda Jimenez <spinedajimenez@csumb.edu>
2024-09-11 19:21:33 -0700

    layout for login

--- Project_1/app/(tabs)/index.tsx
+++ Project_1/app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import Login  from '@/app/login';
 
 export default function HomeScreen() {
   return (
@@ -19,6 +20,7 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
+      <Login />
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
---
+++ Project_1/app/login.tsx
@@ -0,0 +1,70 @@
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+
+const Login: React.FC = () => { 
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = (): void => {
+
+    // login logic with the database
+    console.log('Login attempt with:', username, password);
+    // authenticate the user here
+  };
+  const createAccount =() =>{
+        console.log('Create account');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername} 
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword} 
+      />
+      <Button
+        title="Log In"
+        onPress={handleLogin}
+      />
+      
+      <Button
+        title="Create Account"
+        onPress={createAccount}
+        />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    marginTop: 20
+  },
+  input: {
+    height: 40,
+    marginBottom: 12,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
+});
+
+export default Login;
