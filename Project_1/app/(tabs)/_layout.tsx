6b1b1fa1b97448a25970e01b7cb445cc29dfd465
Alexander Betancourt <AlexanderBBetancourt@proton.me>
2024-09-20 15:56:17 -0700

    Removed my libary.tsx

--- Project_1/app/(tabs)/_layout.tsx
+++ Project_1/app/(tabs)/_layout.tsx
@@ -23,7 +23,7 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="library"
         options={{
           title: 'library',
@@ -31,7 +31,7 @@ export default function TabLayout() {
             <TabBarIcon name={focused ? 'book' : 'book-outline'} color={color} />
           ),
         }}
-      />
+      /> */}
       <Tabs.Screen
         name="home"
         options={{
